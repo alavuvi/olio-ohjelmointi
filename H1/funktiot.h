cf0e854702855c3451a2bee12eef4c568a20369a
alavuvi <ville-pekka.alavuotunki@proton.me>
2024-10-31 10:36:24 +0200

    H1 valmis

---
+++ H1/funktiot.h
@@ -0,0 +1,11 @@
+#ifndef FUNKTIOT_H
+#define FUNKTIOT_H
+using namespace std;
+
+void calcSum(int, int);
+void calcDiv(int, int);
+
+int retSum(int, int);
+float retDiv(int, int);
+
+#endif // FUNKTIOT_H
